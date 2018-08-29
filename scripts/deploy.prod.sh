ce3a733a9e7acea90a95f21cd0badc1edef4fa90
Adam Meyer <adam.meyer@cru.org>
2018-08-29 16:10:44 -0400

    prod deploy bucket fix (#61)

--- scripts/deploy.prod.sh
+++ scripts/deploy.prod.sh
@@ -13,4 +13,4 @@ sed -i "s%$adobePlaceholderTag%$adobeScriptTag%g" dist/knowgod/index.html
 #Deploy to S3
 ~/.local/bin/aws s3 sync dist/knowgod s3://knowgod.com --acl public-read --exclude '*.html' --cache-control 'public, max-age=31536000' --delete
 ~/.local/bin/aws s3 sync dist/knowgod s3://knowgod.com --acl public-read --include '*.html' --cache-control 'public, max-age=120' --delete
-~/.local/bin/aws s3 cp dist/embed.js s3://stage.knowgod.com --acl public-read --cache-control 'public, max-age=600'
+~/.local/bin/aws s3 cp dist/embed.js s3://knowgod.com --acl public-read --cache-control 'public, max-age=600'
