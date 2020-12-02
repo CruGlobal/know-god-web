c1feec48120dd6f4521716f958eef01a7214d078
Scotty Waggoner <ozzieorca@gmail.com>
2020-12-02 11:59:23 -0800

    GT-983: Add GTM page view events

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -2,50 +2,75 @@ import { Injectable } from '@angular/core';
 import { LoaderService } from './loader-service/loader.service';
 import { Router, NavigationEnd } from '@angular/router';
 
+declare global {
+  interface Window {
+    dataLayer: { event: string }[];
+    digitalData: {
+      page?: {
+        pageInfo: {
+          pageName: string;
+          language: string;
+          embedded: 'embed' | '';
+        };
+        category: { primaryCategory: string };
+      };
+    };
+    isEmbedded?: boolean;
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyticsService {
-  constructor(private loaderService: LoaderService, private router: Router) {
-    (<any>window).digitalData = { page: {} };
+  constructor(private router: Router) {
+    window.dataLayer = [];
+    window.digitalData = {};
   }
 
   runAnalyticsInsidePages(url) {
     const [_, language, pageName, pageNumber] = url.split('/');
     this.setDigitalData(pageName || 'knowgod', language, pageNumber);
 
-    if (CustomEvent !== undefined) {
-      const evt = new CustomEvent('content: all pages');
-      document.querySelector('body').dispatchEvent(evt);
-    }
+    this.fireAnalyticsVirtualPageView();
   }
 
   runAnalyticsOnHomepages() {
-    if (CustomEvent !== undefined) {
-      this.router.events.subscribe(event => {
-        if (event instanceof NavigationEnd) {
-          const [_, language, pageName, pageNumber] = event.url.split('/');
-          if (pageNumber === undefined) {
-            this.setDigitalData(pageName || 'knowgod', language);
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const [_, language, pageName, pageNumber] = event.url.split('/');
+        if (pageNumber === undefined) {
+          this.setDigitalData(pageName || 'knowgod', language);
 
-            const evt = new CustomEvent('content: all pages');
-            document.querySelector('body').dispatchEvent(evt);
-          }
+          this.fireAnalyticsVirtualPageView();
         }
-      });
-    }
+      }
+    });
   }
 
   setDigitalData(appName, language, pageNumber?) {
-    (<any>window).digitalData.page = {
+    window.digitalData.page = {
       pageInfo: {
         pageName: `${appName} : ${pageNumber || 'home'}`,
         language: language || 'en',
-        embedded: (<any>window).isEmbedded === true ? 'embed' : ''
+        embedded: window.isEmbedded === true ? 'embed' : ''
       },
       category: {
         primaryCategory: appName
       }
     };
+  }
+
+  fireAnalyticsVirtualPageView() {
+    // Google Analytics
+    window.dataLayer.push({
+      event: 'virtual-page-view'
+    });
+
+    // Adobe Analytics
+    if (CustomEvent !== undefined) {
+      const evt = new CustomEvent('content: all pages');
+      document.querySelector('body').dispatchEvent(evt);
+    }
   }
 }
