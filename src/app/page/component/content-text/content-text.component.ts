dc0fbe18e59b7621f961d6de70c9f7dee460ce73
Caleb Alldrin <caalldrin@gmail.com>
2025-03-14 11:51:34 -0700

    Support text-scale on web

--- src/app/page/component/content-text/content-text.component.ts
+++ src/app/page/component/content-text/content-text.component.ts
@@ -64,7 +64,8 @@ export class ContentTextComponent implements OnChanges {
       )
         ? 'underline'
         : '',
-      'text-align': this.text.textAlign.name || ''
+      'text-align': this.text.textAlign.name || '',
+      'font-size': this.text.textScale + 'em' || ''
       // Do not use color for now since we don't want to support desktop and mobile colors
       // color: this.text.textColor || ''
     };
