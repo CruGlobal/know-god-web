65a789342611cd8a7b833191d0a11b0fd6df2e16
Daniel Bisgrove <daniel@bizz-websites.com>
2025-03-19 16:31:39 -0400

    Cleaning up Dashboard and adding the new /tool/ URL structure

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,6 +5,14 @@ import { delay, take, takeUntil } from 'rxjs/operators';
 import { APIURL } from '../api/url';
 import { CommonService } from '../services/common.service';
 
+interface Resource {
+  imgUrl: string;
+  resourceName: string;
+  id: string;
+  abbreviation: string;
+  tagline: string;
+  resourceType: string;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -18,7 +26,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
   private _booksData: any;
   private _languagesData: any;
 
-  Images = [];
+  resources: Resource[] = [];
   englishLangId = 1;
   englishLangCode = 'en';
   englishLangName = 'English';
@@ -162,7 +170,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
             (included) => included.type === 'attachment'
           );
 
-          const _translations = this._booksData.included.filter(
+          const translations = this._booksData.included.filter(
             (included) =>
               included.type === 'translation' &&
               Number(included.relationships.language.data.id) ===
@@ -176,39 +184,26 @@ export class DashboardComponent implements OnInit, OnDestroy {
                 (o2.attributes['attr-default-order'] || 0)
             )
             .forEach((resource) => {
-              if (resource.attributes['attr-hidden']) {
+              const { id: resourceId, attributes, relationships } = resource;
+              const resourceType = attributes['resource-type'];
+
+              if (attributes['attr-hidden']) {
                 return;
               }
-              if (
-                !this.webContentTypes.includes(
-                  resource.attributes['resource-type']
-                )
-              ) {
+              if (!this.webContentTypes.includes(resourceType)) {
                 return;
               }
 
-              if (resource.attributes['resource-type'] === 'cyoa') {
-                console.log('CYOA', resource);
-              }
-
-              const resourceId = resource.id;
-              const bannerId = resource.attributes['attr-banner'];
-
-              if (
-                resource.relationships.metatool &&
-                resource.relationships.metatool.data
-              ) {
-                const metatoolId = resource.relationships.metatool.data.id;
-                const metatool =
-                  this._booksData.data.find((t) => t.id === metatoolId) ||
+              if (relationships?.metatool?.data) {
+                const metaToolId = relationships.metatool.data.id;
+                const metaTool =
+                  this._booksData.data.find((tool) => tool.id === metaToolId) ||
                   this._booksData.included.find(
-                    (t) => t.type === 'resource' && t.id === metatoolId
+                    (tool) => tool.type === 'resource' && tool.id === metaToolId
                   );
 
                 const defaultVariant =
-                  metatool != null
-                    ? metatool.relationships['default-variant']
-                    : null;
+                  metaTool?.relationships['default-variant'] || null;
                 if (
                   defaultVariant != null &&
                   defaultVariant.data.id !== resourceId
@@ -217,26 +212,30 @@ export class DashboardComponent implements OnInit, OnDestroy {
                 }
               }
 
-              const _tTranslation = _translations.find(
+              const translation = translations.find(
                 (x) => x.relationships.resource.data.id === resourceId
               );
 
-              if (_tTranslation && _tTranslation.attributes) {
-                const _tTranslatedName =
-                  _tTranslation.attributes['translated-name'];
-                const _tTranslatedTagLine =
-                  _tTranslation.attributes['translated-tagline'];
+              if (translation?.attributes) {
+                const resourceName = translation.attributes['translated-name'];
+                const tagline = translation.attributes['translated-tagline'];
+                const imgUrl = attachments.find(
+                  (x) => x.id === attributes['attr-banner']
+                )?.attributes.file;
 
-                this.Images.push({
-                  ImgUrl: attachments.find((x) => x.id === bannerId)?.attributes
-                    .file,
-                  resource: _tTranslatedName,
-                  id: resourceId,
-                  abbreviation: resource.attributes.abbreviation,
-                  tagline: _tTranslatedTagLine
-                });
+                this.resources = [
+                  ...this.resources,
+                  {
+                    imgUrl,
+                    resourceName,
+                    id: resourceId,
+                    abbreviation: attributes.abbreviation,
+                    tagline,
+                    resourceType
+                  }
+                ];
               } else {
-                console.log('MISSING TRANSLATION', resource, _tTranslation);
+                console.log('MISSING TRANSLATION', resource, translation);
               }
             });
 
@@ -245,8 +244,15 @@ export class DashboardComponent implements OnInit, OnDestroy {
       });
   }
 
-  navigateToPage(abbreviation) {
-    this.route.navigateByUrl(this.dispLanguageCode + '/' + abbreviation);
+  navigateToPage(resource: Resource): void {
+    const abbreviation = resource.abbreviation;
+    const urlResourceType = resource.resourceType === 'tract' ? 't' : 'c';
+    const bookType = this.webContentTypes.includes(resource.resourceType)
+      ? 'tool'
+      : 'lesson';
+
+    const url = `${this.dispLanguageCode}/${bookType}/${urlResourceType}/${abbreviation}`;
+    this.route.navigateByUrl(url);
   }
 
   onSwitchLanguage(): void {
@@ -256,7 +262,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
   onSelectLanguage(pLangCode: string): void {
     this.sectionReady = false;
     this._languageChanged.next();
-    this.Images = [];
+    this.resources = [];
     this.langSwitchOn = !this.langSwitchOn;
     this.route.navigate(['/', pLangCode]);
   }
