1c00c7528c9779c4ae07f393f89f9ff9ac0556ed
Caleb Alldrin <caalldrin@gmail.com>
2025-03-20 10:24:23 -0700

    Implement textScale and minimumLines

--- src/app/page/component/content-text/content-text.component.ts
+++ src/app/page/component/content-text/content-text.component.ts
@@ -65,7 +65,9 @@ export class ContentTextComponent implements OnChanges {
         ? 'underline'
         : '',
       'text-align': this.text.textAlign.name || '',
-      'font-size': this.text.textScale ? `${this.text.textScale}rem` : ''
+      'font-size': this.text.textScale ? `${this.text.textScale}rem` : '',
+      'line-height': this.text.textScale ? `${this.text.textScale}rem` : '',
+      'min-height': this.text.minimumLines ? `${this.text.minimumLines}em` : ''
       // Do not use color for now since we don't want to support desktop and mobile colors
       // color: this.text.textColor || ''
     };
